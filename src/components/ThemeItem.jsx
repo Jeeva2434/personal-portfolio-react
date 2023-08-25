61e959cc86651428a009c5469b7474066bac9cdc
jeeva <jeevanandam.s@heptagon.in>
2023-08-25 13:12:32 +0530

    portfolio layout final

--- src/components/ThemeItem.jsx
+++ src/components/ThemeItem.jsx
@@ -1,14 +1,15 @@
 import PropTypes from "prop-types";
 
-const ThemeItem = ({color,img}) => {
+const ThemeItem = ({color,img,changeColor}) => {
   return (
-    <img src={img} alt="" className="theme__img"/>
+    <img src={img} alt="" className="theme__img" onClick={()=>changeColor(color)}/>
   )
 }
 
 ThemeItem.propTypes = {
     color: PropTypes.string,
-    img: PropTypes.string
+    img: PropTypes.string,
+    changeColor : PropTypes.func
 }
 
 
